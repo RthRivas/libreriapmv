847d95c3d63c281d17abafdabf6001a79cd2c1a1
RthRivas <38531899+RthRivas@users.noreply.github.com>
2019-02-19 19:07:19 -0600

    Add files via upload

---
+++ Libreria/ventas.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <style type="text/css">
+
+  	#contenedor 
+  	{
+  			padding-left: 280px;
+  			height: 100px;
+  			width: 1400px;
+
+	}
+	#grafic {
+		width:350px; 
+		height:350px;
+	}
+
+  </style>
+</head>
+<body>
+	<div id="contenedor">
+		<div role="tabpanel" class="tab-pane fade active in" id="" aria-labelledby="-tab">
+			<div class="col-xs-12 col-sm-6 col-md-3">
+	            <div class="thumbnail">
+	                 <a href=""><img id="grafic" src="img/venta1.png" ></a>
+	            </div>
+	        </div> 
+	    </div>
+	    <div role="tabpanel" class="tab-pane fade active in" id="" aria-labelledby="-tab">   
+	        <div class="col-xs-12 col-sm-6 col-md-3">
+	            <div class="thumbnail">
+	                 <a href=""><img id="grafic" src="img/venta2.png" ></a>
+	            </div>
+	        </div>
+	    </div><br>
+	    <div role="tabpanel" class="tab-pane fade active in" id="" aria-labelledby="-tab">     
+	        <div class="col-xs-12 col-sm-6 col-md-3">
+	            <div class="thumbnail">
+	                 <a href=""><img id="grafic" src="img/venta3.png" ></a>
+	            </div>
+	        </div>
+	    </div>      
+	</div>
+</body>
+</html>
