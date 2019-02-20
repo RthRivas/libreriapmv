0890aace16a2532d2a5b741ae7677ef42a473cbb
RthRivas <38531899+RthRivas@users.noreply.github.com>
2019-02-19 20:19:14 -0600

    Update Ventas.php

--- Ventas.php
+++ Ventas.php
@@ -12,7 +12,7 @@
 
 	}
 	#grafic {
-		width:450px; 
+		width:500px; 
 		height:350px;
 	}
 
@@ -20,21 +20,23 @@
 </head>
 <body>
 	<div id="contenedor">
-		<div role="tabpanel" id="" aria-labelledby="-tab">
+		<div role="tabpanel" >
 			<div class="col-xs-12 col-sm-6 col-md-3">
 	            <div class="thumbnail">
 	                 <a href=""><img id="grafic" src="img/venta1.png" ></a>
 	            </div>
 	        </div> 
 	    </div>
-	    <div role="tabpanel" id="" aria-labelledby="-tab">   
+		
+	    <div role="tabpanel">   
 	        <div class="col-xs-12 col-sm-6 col-md-3">
 	            <div class="thumbnail">
 	                 <a href=""><img id="grafic" src="img/venta2.png" ></a>
 	            </div>
 	        </div>
-	    </div><br>
-	    <div role="tabpanel" id="" aria-labelledby="-tab">     
+	    </div>
+		
+	    <div role="tabpanel">     
 	        <div class="col-xs-12 col-sm-6 col-md-3">
 	            <div class="thumbnail">
 	                 <a href=""><img id="grafic" src="img/venta3.png" ></a>
